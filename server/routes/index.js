a897b44b0a1181c61dba27740db58f4a526059df
shikhar-priyadarshi-vst-au4 <officialshikharid@gmail.com>
2020-05-28 12:07:24 +0530

    second commit: backend

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const surveys = require("../public/survey.json");
+const employees = require("../public/employees.json");
+
+router.get("/survey-data", (req, res) => {
+  let survey = JSON.parse(JSON.stringify(surveys));
+  if (!!survey.length) {
+    res.status(200).json(survey);
+  } else {
+    res.status(400).json({
+      status: "failure",
+      message: "failed to fetch",
+    });
+  }
 });
 
+router.get("/employee-data", (req, res) => {
+  let employee = JSON.parse(JSON.stringify(employees));
+  if (!!employee.length) {
+    res.status(200).json(employee);
+  } else {
+    res.status(400).json({
+      status: "failure",
+      message: "failed to fetch",
+    });
+  }
+});
 module.exports = router;
